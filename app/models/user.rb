886e56d7ffb6a5d88c740448f1d6849056be2d20
mmhm416 <tma.hshmt@gmail.com>
2021-05-01 13:01:05 +0900

    ヴァリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+        with_options presence: true do
+          validates :nickname
+          validates :birthday
+          with_options uniqueness: true do
+            validates :nickname
+            validates :email
+          end
+          with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."} do
+          validates :first_name
+          validates :last_name
+          end
+          with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+          validates :first_name_kana
+          validates :last_name_kana
+          end
+        end
 end
