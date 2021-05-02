64445cb8bc00077b10f4604c0b3f38262ef9c421
mmhm416 <tma.hshmt@gmail.com>
2021-05-02 12:43:59 +0900

    rubcop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-        with_options presence: true do
-          validates :nickname
-          validates :birthday
-          with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters"} do
-            validates :first_name
-            validates :last_name
-          end
-          with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"} do
-            validates :first_name_kana
-            validates :last_name_kana
-          end
-        end
-        with_options uniqueness: { case_sensitive: true } do
-          validates :nickname
-          validates :email
-        end
-        with_options format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Password Include both letters and numbers"} do
-          validates :password
-        end
 
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'input full-width characters' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'input full-width katakana characters' } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
   end
+  with_options uniqueness: { case_sensitive: true } do
+    validates :nickname
+    validates :email
+  end
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'include both letters and numbers' } do
+    validates :password
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :user do
+    nickname              { 'ふりま太郎' }
+    email                 { Faker::Internet.free_email }
+    password              { 'test1234' }
+    password_confirmation { password }
+    first_name            { 'ふりま' }
+    last_name             { '太郎' }
+    first_name_kana       { 'フリマ' }
+    last_name_kana        { 'タロウ' }
+    birthday              { '1980-5-3' }
   end
 end
