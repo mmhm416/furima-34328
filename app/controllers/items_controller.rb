37337e0f5746d3e06759f9f5f76e8899ed1a5f43
mmhm416 <tma.hshmt@gmail.com>
2021-05-04 16:45:54 +0900

    エラー文表示、アイテム保存機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,29 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :delivery_cost_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :delivery_time_id, numericality: { other_than: 1 } 
+  with_options numericality: { other_than: 0, message: 'Select' } do
+  validates :category_id
+  validates :condition_id
+  validates :delivery_cost_id
+  validates :prefecture_id
+  validates :delivery_time_id
+  end
 
   with_options presence: true do
     validates :item_name
     validates :explanation
     validates :price
+    validates :image
   end
 
+  validates :price, format: { with: /\A[\d]+\z/}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   has_one_attached :image
-  
+
   belongs_to :user
   
 end
