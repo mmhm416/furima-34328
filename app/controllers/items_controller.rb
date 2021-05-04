f6ae134a6dc61a13f454bf63253e155862c6d20b
mmhm416 <tma.hshmt@gmail.com>
2021-05-04 17:01:01 +0900

    未ログイン状態の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
   end
 
@@ -15,8 +17,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  private
   def item_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
