b82f6b8805354401eb9f14e9c616570042e4d1d3
mmhm416 <tma.hshmt@gmail.com>
2021-05-04 11:03:54 +0900

    画像投稿機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,8 @@ class Item < ApplicationRecord
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
+  has_one_attached :image
+  
   belongs_to :user
   
 end
