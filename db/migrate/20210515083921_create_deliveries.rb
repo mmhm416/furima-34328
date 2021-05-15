742797f3dead65debb47d6ad0486ce02789018f0
mmhm416 <tma.hshmt@gmail.com>
2021-05-15 17:50:19 +0900

    マイグレーション、DB作成

--- db/migrate/20210515083921_create_deliveries.rb
+++ db/migrate/20210515083921_create_deliveries.rb
@@ -1,7 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
-
+      t.string     :zip_code,        null: false
+      t.integer    :prefecture_id,   null: false
+      t.string     :city,            null: false
+      t.string     :adress,          null: false
+      t.string     :building,        
+      t.string     :phone_no,        null: false
+      t.references :purchase,        null: false, foreign_key: true
       t.timestamps
     end
   end
