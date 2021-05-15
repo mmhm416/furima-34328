a2942330f4dcbef29356c064f5e9532fbee91d7d
mmhm416 <tma.hshmt@gmail.com>
2021-05-15 18:44:08 +0900

    migrationやり直し

--- db/migrate/20210515083921_create_deliveries.rb
+++ db/migrate/20210515083921_create_deliveries.rb
@@ -5,7 +5,7 @@ class CreateDeliveries < ActiveRecord::Migration[6.0]
       t.integer    :prefecture_id,   null: false
       t.string     :city,            null: false
       t.string     :adress,          null: false
-      t.string     :building,        
+      t.string     :building
       t.string     :phone_no,        null: false
       t.references :purchase,        null: false, foreign_key: true
       t.timestamps
