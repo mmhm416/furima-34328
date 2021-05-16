091cfb7556c6be72be1d8621ddde88383dda408c
mmhm416 <tma.hshmt@gmail.com>
2021-05-16 21:13:47 +0900

    formオブジェクトを用いたDB保存実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,23 @@
 class PurchasesController < ApplicationController
-  def new
-  end
 
   def create
+    @purchase_delivery = PurchaseDelivery.new(purchase_params)
+    if @purchase_delivery.valid?
+      @purchase_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
-  
+
   def index
+    @purchase_delivery = PurchaseDelivery.new
+    @item = Item.find(params[:item_id])
   end
-  
+
+end
+
+private
+def purchase_params
+  params.require(:purchase_delivery).permit(:zip_code, :prefecture_id, :city, :address, :building, :phone_no).merge(user_id: current_user.id, item_id: params[:item_id])
 end
--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -1,12 +1,12 @@
 class PurchaseDelivery
   
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :zip_code, :prefecture_id, :city, :adress, :building, :phone_no, :purchase_id
+  attr_accessor :user_id, :item_id, :zip_code, :prefecture_id, :city, :address, :building, :phone_no, :purchase_id
 
   with_options presence: true do
     validates :zip_code
     validates :city
-    validates :adress
+    validates :address
     validates :phone_no
   end
 
@@ -14,7 +14,7 @@ class PurchaseDelivery
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Delivery.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, adress: adress, building: building, phone_no: phone_no, purchase_id: purchase.id)
+    Delivery.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_no: phone_no, purchase_id: purchase.id)
   end
 
 end
